910bcbb232434801eed1d037fce59c94897f7d6f
bergb15 <brianvdberg2107@gmail.com>
2023-11-10 21:04:29 +0100

    Added args and stop to periodic task

--- internal/timing.js
+++ internal/timing.js
@@ -5,15 +5,26 @@
 */
 
 /**
+ * Makes the task inactive for a specified duration.
+ *
+ * @param {number} ms - The duration in milliseconds for which the task should be inactive.
+ * @returns {Promise<void>} A promise that resolves after the specified duration.
+ */
+export function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
  * Schedules a task to run periodically with a specified interval.
  *
  * @param {Function} task - The task to be scheduled.
+ * @param {Array<any>} args - An array of arguments to be passed to the task function.
  * @param {number} interval - The interval in milliseconds between each execution of the task.
  * @returns {number} The ID of the interval, which can be used to clearInterval.
  */
-export function schedulePeriodicTask(task, interval) {
+export function startPeriodicTask(task, args, interval) {
     return setInterval(async () => {
-        const result = task();
+        const result = task(...args);
 
         if (result instanceof Promise) {
             await result; // Wait for the promise to resolve
@@ -22,11 +33,11 @@ export function schedulePeriodicTask(task, interval) {
 }
 
 /**
- * Makes the task inactive for a specified duration.
+ * Stops a periodically running task.
  *
- * @param {number} ms - The duration in milliseconds for which the task should be inactive.
- * @returns {Promise<void>} A promise that resolves after the specified duration.
+ * @param {number} intervalID - The ID of the interval to be cleared.
+ * @returns {void}
  */
-export function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+export function stopPeriodicTask(intervalID) {
+    clearInterval(intervalID);
 }
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+import { schedulePeriodicTask, sleep } from './internal/timing';
+
+function HelloWorld(func) {
+    func("Hello world!")
+}
+
 export async function main(ns) {
-    
+    schedulePeriodicTask(HelloWorld, [ns.tprint], 1000);
+    while(true) {
+        await sleep(10000)
+    }
 }
