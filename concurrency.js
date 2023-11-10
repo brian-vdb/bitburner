435f4307e218f6ccfd7da40fd4513f8908295a69
bergb15 <brianvdberg2107@gmail.com>
2023-11-10 17:35:49 +0100

    Extracted unnecesary error throw

--- concurrency.js
+++ concurrency.js
@@ -1,15 +1,14 @@
 // getNumLogicalProcessors gets the number of logical processors for optimally offloading to workers
-export async function getNumLogicalProcessors(ns) {
+export async function getNumLogicalProcessors() {
     try {
         // Fetch the number logical processors available
         if (navigator && 'hardwareConcurrency' in navigator) {
             const numLogicalProcessors = navigator.hardwareConcurrency || 1;
             return numLogicalProcessors;
         } else {
-            throw new Error('Navigator or hardwareConcurrency not available');
+            return 1; // Default value if Navigator or hardwareConcurrency not available
         }
     } catch (error) {
-        ns.tprint(`Error: ${error.message}`);
         return 1; // Default value if an error occurs
     }
 }
