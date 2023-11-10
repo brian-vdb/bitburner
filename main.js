17b1b5b7c3e2347d6a57a8397fc4646b9ff96070
bergb15 <brianvdberg2107@gmail.com>
2023-11-10 21:39:53 +0100

    Emptied the base main

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import { schedulePeriodicTask, sleep } from './internal/timing';
-
-function HelloWorld(func) {
-    func("Hello world!")
-}
-
 export async function main(ns) {
-    schedulePeriodicTask(HelloWorld, [ns.tprint], 1000);
-    while(true) {
-        await sleep(10000)
-    }
+
 }
