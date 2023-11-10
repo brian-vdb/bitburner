f1a465835b710742a4d81bf1aee1c163067b5354
bergb15 <brianvdberg2107@gmail.com>
2023-11-10 23:44:16 +0100

    Added intrusion and tracking

---
+++ PropagationAttack/files.js
@@ -0,0 +1,22 @@
+/*
+   Brian van den Berg
+   File: files.js
+   Description: This file contains functions related to saving the hostnames of servers.
+*/
+
+/**
+ * Saves an array of server hostnames as a JSON format in a .txt file
+ *
+ * @param {import("../index").NS} ns - The environment.
+ * @param {string[]} hostnames - An array of server hostnames.
+ * @returns {void}
+ */
+export function saveHostnamesAsJSON(ns, hostnames) {
+    if (!hostnames || hostnames.length === 0) {
+        ns.tprint("No hostnames provided to store.");
+        return;
+    }
+
+    const jsonContent = JSON.stringify(hostnames.map(hostname => ({ hostname })), null, 2);
+    ns.write("hostnames.txt", jsonContent);
+}
---
+++ PropagationAttack/main.js
@@ -0,0 +1,53 @@
+/*
+   Brian van den Berg
+   Module: PropagationAttack
+   File: main.js
+   Description: This module contains functions related to network propagation and intrusion for the PropagationAttack feature.
+*/
+
+import { getAvailableHacks, intrudeServer } from "./PropagationAttack/intrude";
+import { saveHostnamesAsJSON } from "./PropagationAttack/files";
+
+/**
+ * Propagates through the network and all of its nodes.
+ *
+ * @param {import("../index").NS} ns - The environment.
+ * @param {string} host - The hostname of the server currently being propagated from
+ * @param {string[]} hostnames - The list of hostnames that have been seen
+ * @returns {string[]} An array of server hostnames on the network.
+ */
+function _propagateNetwork(ns, host, hostnames) {
+    hostnames.push(host);
+
+    // Find the neighboring servers
+    const targets = ns.scan(host);
+
+    // Using forEach to iterate over the array
+    targets.forEach((target) => {
+        if (!hostnames.includes(target)) {
+            hostnames = _propagateNetwork(ns, target, hostnames);
+        }
+    });
+
+    return hostnames;
+}
+
+/** @param {import("../index").NS} ns */
+export async function main(ns) {
+    const host = ns.getHostname();
+    let hostnames = [];
+    
+    // Get a list of all of the hostnames in the whole network
+    hostnames = _propagateNetwork(ns, host, hostnames);
+
+    // Save the list of hostnames for future reference
+    saveHostnamesAsJSON(ns, hostnames);
+
+    // Get the list of available hacking methods
+    const hacks = getAvailableHacks(ns);
+
+    // Hack into every vulnerable server in the network
+    hostnames.forEach((hostname) => {
+        intrudeServer(ns, hacks, hostname);
+    });
+}
