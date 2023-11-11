1a55658116c0dba979655ea25bcb01dfd4966dec
bergb15 <brianvdberg2107@gmail.com>
2023-11-11 19:02:24 +0100

    Split the predefined hacking functions from main

--- main.js
+++ main.js
@@ -8,45 +8,6 @@ import { isProcessRunning } from './internal/process';
 import { sleep } from './internal/time';
 
 /**
- * Weakens a server from a host with a number of threads.
- *
- * @param {import("./index").NS} ns - The environment object.
- * @param {string} hostname - Server hosting the attack.
- * @param {string} target - Server to attack.
- * @param {number} threads - Number of threads to attack with.
- * @returns {boolean} - True if the process started.
- */
-function weaken(ns, hostname, target, threads) {
-    return ns.exec('public/weaken.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
-}
-
-/**
- * Grows a server from a host with a number of threads.
- *
- * @param {import("./index").NS} ns - The environment object.
- * @param {string} hostname - Server hosting the attack.
- * @param {string} target - Server to attack.
- * @param {number} threads - Number of threads to attack with.
- * @returns {boolean} - True if the process started.
- */
-function grow(ns, hostname, target, threads) {
-    return ns.exec('public/grow.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
-}
-
-/**
- * Hacks a server from a host with a number of threads.
- *
- * @param {import("./index").NS} ns - The environment object.
- * @param {string} hostname - Server hosting the attack.
- * @param {string} target - Server to attack.
- * @param {number} threads - Number of threads to attack with.
- * @returns {boolean} - True if the process started.
- */
-function hack(ns, hostname, target, threads) {
-    return ns.exec('public/hack.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
-}
-
-/**
  * Main function to automate the game.
  *
  * @param {import("./index").NS} ns - The environment object.
---
+++ network/hacks.js
@@ -0,0 +1,44 @@
+/*
+   Brian van den Berg
+   File: hacks.js
+   Description: This file contains functions to hack a server.
+*/
+
+/**
+ * Weakens a server from a host with a number of threads.
+ *
+ * @param {import("./index").NS} ns - The environment object.
+ * @param {string} hostname - Server hosting the attack.
+ * @param {string} target - Server to attack.
+ * @param {number} threads - Number of threads to attack with.
+ * @returns {boolean} - True if the process started.
+ */
+function weaken(ns, hostname, target, threads) {
+    return ns.exec('public/weaken.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
+}
+
+/**
+ * Grows a server from a host with a number of threads.
+ *
+ * @param {import("./index").NS} ns - The environment object.
+ * @param {string} hostname - Server hosting the attack.
+ * @param {string} target - Server to attack.
+ * @param {number} threads - Number of threads to attack with.
+ * @returns {boolean} - True if the process started.
+ */
+function grow(ns, hostname, target, threads) {
+    return ns.exec('public/grow.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
+}
+
+/**
+ * Hacks a server from a host with a number of threads.
+ *
+ * @param {import("./index").NS} ns - The environment object.
+ * @param {string} hostname - Server hosting the attack.
+ * @param {string} target - Server to attack.
+ * @param {number} threads - Number of threads to attack with.
+ * @returns {boolean} - True if the process started.
+ */
+function hack(ns, hostname, target, threads) {
+    return ns.exec('public/hack.js', hostname, {preventDuplicates: false, threads: threads}, target, threads) > 0;
+}
