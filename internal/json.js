8facc8d3c380a939e95be0eaf58a0ae62f01bc23
bergb15 <brianvdberg2107@gmail.com>
2023-11-11 18:49:30 +0100

    Added a fieldname to the save function

--- internal/json.js
+++ internal/json.js
@@ -9,16 +9,17 @@
  *
  * @param {import("../index").NS} ns - The environment object.
  * @param {Array} data - An array of data to be saved as JSON.
+ * @param {string} fieldname - Name of the value in the array
  * @param {string} filename - The name of the file to be written.
  * @returns {void}
  */
-export function saveArrayAsJSON(ns, data, filename) {
+export function saveArrayAsJSON(ns, data, fieldname, filename) {
     if (!data || data.length === 0) {
         ns.tprint("No data provided to store.");
         return;
     }
 
-    const jsonContent = data.map(item => JSON.stringify({ item }, null, 2)).join('\n');
+    const jsonContent = data.map(item => JSON.stringify({ [fieldname]: item }, null, 2)).join('\n');
     ns.write(filename || "data.txt", jsonContent, "w");
     ns.tprint(`Data stored in '${filename || "data.txt"}' as JSON.`);
 }
