2b401fb760430c106dbde738483b16ddebda31ba
bergb15 <brianvdberg2107@gmail.com>
2023-11-12 13:39:15 +0100

    Reformatted the hack scripts

--- public/hack.js
+++ public/hack.js
@@ -5,9 +5,9 @@
  * @returns {Promise<void>} A promise that resolves when the script finishes.
  */
 export async function main(ns) {
-    const hostname = ns.args[0];
-    const threads = parseInt(ns.args[1], 10);
+  const hostname = ns.args[0];
+  const threads = parseInt(ns.args[1], 10);
 
-    // Perform the grow attack
-    await ns.hack(hostname, {threads: threads});
+  // Perform the grow attack
+  await ns.hack(hostname, { threads: threads });
 }
