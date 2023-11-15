7207eb8600a601a66ef7d81400d6039a1cbaf21b
bergb15 <brianvdberg2107@gmail.com>
2023-11-15 19:59:15 +0100

    Added errors to tool handles

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
    Description: This file contains functionality related to automation.
 */
 
-import { propagationAttack, serverAnalysis } from "./tools/handles";
+import { propagationAttack, serverAnalysis, batchAnalysis } from "./tools/handles";
 
 /**
  * Main function to automate the game.
@@ -13,10 +13,15 @@ import { propagationAttack, serverAnalysis } from "./tools/handles";
  * @returns {Promise<void>} A promise that resolves when the script finishes.
  */
 export async function main(ns) {
-  ns.tprint(`>>>\n| Running: Propagation Attack |`);
+  // Perform a Propagation Attack
+  ns.tprint(`>>> | Running: Propagation Attack | <<<`);
   await propagationAttack(ns);
 
-  let inputFile = "data/servers.txt";
-  ns.tprint(`>>>\n| Running: Server Analysis [${inputFile}] |`);
-  await serverAnalysis(ns, inputFile);
+  // Perform a Server Analysis
+  ns.tprint(`>>> | Running: Server Analysis | <<<`);
+  await serverAnalysis(ns);
+
+  // Perform a Batch Analysis
+  ns.tprint(`>>> | Running: Batch Analysis | <<<`);
+  await batchAnalysis(ns);
 }
--- tools/handles.js
+++ tools/handles.js
@@ -14,34 +14,29 @@ import { awaitScript } from "./internal/process";
  */
 export async function propagationAttack(ns) {
   // Start the attack
-  const pid = ns.exec(
-    "./tools/PropagationAttack/main.js",
-    ns.getHostname(),
-    {
-      preventDuplicates: true,
-      temporary: false,
-      threads: 1,
-    }
-  );
+  const pid = ns.exec("./tools/PropagationAttack/main.js", ns.getHostname(), {
+    preventDuplicates: true,
+    temporary: false,
+    threads: 1,
+  });
 
-  // Check if the process was started
-  if (pid == 0) {
-    return false;
-  } else {
-    await awaitScript(ns, pid);
-    return true;
-  }
+  // Check if the process started
+  if (pid == 0) throw new Error(`Propagation Attack could not be started`);
+  
+  // Wait for the process to finish
+  await awaitScript(ns, pid);
+  
+  return 0;
 }
 
 /**
- * Start a server analysis using data/servers.txt to split it into hosts and targets.
- * The result gets stored in hosts.txt and targets.txt.
+ * Start server analysis using data/servers.txt.
+ * The result gets stored in data/hosts.txt and data/targets.txt.
  *
  * @param {import("../index").NS} ns - The environment object.
- * @param {string} inputFile - File containing server objects with hostnames.
  * @returns {Promise<boolean>} - True if the process started.
  */
-export async function serverAnalysis(ns, inputFile) {
+export async function serverAnalysis(ns) {
   // Start the analysis
   const pid = ns.exec(
     "./tools/ServerAnalysis/main.js",
@@ -51,14 +46,43 @@ export async function serverAnalysis(ns, inputFile) {
       temporary: false,
       threads: 1,
     },
-    inputFile
+    "data/servers.txt"
   );
 
-  // Check if the process was started
-  if (pid == 0) {
-    return false;
-  } else {
-    await awaitScript(ns, pid);
-    return true;
-  }
+  // Check if the process started
+  if (pid == 0) throw new Error(`Server Analysis could not be started`);
+
+  // Wait for the process to finish
+  await awaitScript(ns, pid);
+
+  return 0;
+}
+
+/**
+ * Start batch analysis using data/targets.txt.
+ * The result gets stored in data/batches.txt.
+ *
+ * @param {import("../index").NS} ns - The environment object.
+ * @returns {Promise<boolean>} - True if the process started.
+ */
+export async function batchAnalysis(ns, inputFile) {
+  // Start the analysis
+  const pid = ns.exec(
+    "./tools/BatchAnalysis/main.js",
+    ns.getHostname(),
+    {
+      preventDuplicates: true,
+      temporary: false,
+      threads: 1,
+    },
+    "data/targets.txt"
+  );
+
+  // Check if the process started
+  if (pid == 0) throw new Error(`Batch Analysis could not be started`);
+
+  // Wait for the process to finish
+  await awaitScript(ns, pid);
+
+  return 0;
 }
